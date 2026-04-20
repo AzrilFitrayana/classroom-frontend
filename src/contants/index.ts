a56262cbd27cc47e3c378d2d6d6362d81ecd1a42
azril fitrayana <azrilfitrayana77@gmail.com>
2026-04-20 16:35:22 +0700

    implement data provider

---
+++ src/contants/index.ts
@@ -0,0 +1,10 @@
+export const DEPARTEMENTS = [
+  "Computer Science",
+  "Mathematics",
+  "Software Engineering",
+];
+
+export const DEPARTMENT_OPTIONS = DEPARTEMENTS.map((dept) => ({
+  value: dept,
+  label: dept,
+}));
