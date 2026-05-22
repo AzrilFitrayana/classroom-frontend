9b243a0fff7f5257a443413b56baf6fc614d409b
azril fitrayana <azrilfitrayana77@gmail.com>
2026-05-22 19:36:03 +0700

    implement detail class page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -19,6 +19,7 @@ import SubjectsList from "./pages/subjects/list";
 import SubjectsCreate from "./pages/subjects/create";
 import ClassesList from "./pages/classes/list";
 import ClassesCreate from "./pages/classes/create";
+import ClassesShow from "./pages/classes/show";
 
 function App() {
   return (
@@ -51,6 +52,7 @@ function App() {
                   name: 'classes',
                   list: '/classes',
                   create: '/classes/create',
+                  show: '/classes/show/:id',
                   meta: { label: 'Classes', icon: <GraduationCap /> }
                 }
               ]}
@@ -71,6 +73,7 @@ function App() {
                   <Route path="/classes">
                     <Route index element={<ClassesList />} />
                     <Route path="create" element={<ClassesCreate />} />
+                    <Route path="show/:id" element={<ClassesShow />} />
                   </Route>
                 </Route>
               </Routes>
